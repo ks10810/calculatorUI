7f443b02d73f3474c3875c4ca8ccfdf1bfea54b9
aaa <123@gmail.com>
2019-09-10 11:13:07 -0700

    first commit

---
+++ src/main/java/com/example/calculatorui/CalculatoruiApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculatorui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatoruiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatoruiApplication.class, args);
+    }
+
+}
